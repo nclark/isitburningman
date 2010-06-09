2177f3cf5dabda03e1e8abe34869edd55c621782
Adam Patten <adampatten@kidzui.com>
2010-06-09 08:11:05 +0800

    is_it_burningman? should return true/false not magic
    * use yahoo service to serve css

--- app.rb
+++ app.rb
@@ -1,17 +1,10 @@
 def is_it_burningman?
   burningman_week = ["2010-08-30","2010-08-31","2010-09-01","2010-09-02","2010-09-03","2010-09-04","2010-09-05","2010-09-06"]
   
-  burningman_week.each do |day|
-    if Date.today.to_s.match day
-      @answer = "YES!"
-      return
-    else
-      @answer = "NO"
-    end
-  end
+  burningman_week.each { |d| return true if Date.today.to_s.match(d) }
+  false
 end
 
 get '/' do
-  is_it_burningman?
-  haml :index, :locals => { :answer => @answer }
+  haml :index, :locals => { :answer => is_it_burningman? ? 'YES!' : 'NO' }
 end
