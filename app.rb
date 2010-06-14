031ac5841f1ca96432875da726196b561aeb7f5e
Jay Swain <github@jayswain.com>
2010-06-13 23:43:44 -0700

    * clean up is_it_burning_man

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
-def is_it_burningman?
-  burningman_week = ["2010-08-30","2010-08-31","2010-09-01","2010-09-02","2010-09-03","2010-09-04","2010-09-05","2010-09-06"]
-  
-  burningman_week.each { |d| return true if Date.today.to_s.match(d) }
-  false
+def is_it_burning_man?
+  (Date.parse('2010-08-30')..Date.parse('2010-09-06')).member?(Date.today)
 end
 
 get '/' do
-  haml :index, :locals => { :answer => is_it_burningman? ? 'YES!' : 'NO' }
+  haml :index, :locals => { :answer => is_it_burning_man? ? 'YES!' : 'NO' }
 end
